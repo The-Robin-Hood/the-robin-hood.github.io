e8e4d13756ed6a7497ee1fa4571ce52ef2cc173e
Ansari <ansarithameem674@gmail.com>
2022-02-14 17:54:05 +0530

    Delete markdown directory

--- markdown/index.js
+++
@@ -1,7 +0,0 @@
-var converter = new showdown.Converter();
-const textArea = document.getElementById("text-area");
-const previewArea = document.getElementById("preview-area");
-textArea.addEventListener("input", () => {
-  const txt = textArea.value;
-  previewArea.innerHTML = converter.makeHtml(txt);
-});
